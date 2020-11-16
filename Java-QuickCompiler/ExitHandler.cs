17820302b68726b38f2da34920d825eb36234e23
Yannick Fricke <yannickfricke@googlemail.com>
2020-11-16 12:24:17 +0100

    Removed unused imports

--- Java-QuickCompiler/ExitHandler.cs
+++ Java-QuickCompiler/ExitHandler.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Text;
 
 
 // https://www.meziantou.net/detecting-console-closing-in-dotnet.htm
--- Java-QuickCompiler/Program.cs
+++ Java-QuickCompiler/Program.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
-using System.Timers;
 
 namespace J
 {
